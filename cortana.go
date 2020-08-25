16014e6ed8fbb452863d0f756430486e055334cd
shafreeck <shafreeck@gmail.com>
2020-08-26 01:45:24 +0800

    improve the output of usage

--- cortana.go
+++ cortana.go
@@ -315,7 +315,9 @@ func (c *Cortana) checkRequires(args []string, v interface{}) {
 	if i < len(nonflags) {
 		for _, nf := range nonflags[i:] {
 			if nf.required {
-				fatal(errors.New(nf.long + " is required"))
+				fmt.Println(nf.long + " is required")
+				fmt.Println()
+				c.Usage()
 			}
 		}
 
@@ -338,10 +340,14 @@ func (c *Cortana) checkRequires(args []string, v interface{}) {
 		}
 
 		if f.long != "-" {
-			fatal(errors.New(f.long + " is required"))
+			fmt.Println(f.long + " is required")
+			fmt.Println()
+			c.Usage()
 		}
 		if f.short != "-" {
-			fatal(errors.New(f.short + " is required"))
+			fmt.Println(f.short + " is required")
+			fmt.Println()
+			c.Usage()
 		}
 	}
 }
